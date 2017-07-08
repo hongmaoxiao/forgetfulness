87cbfce570c528aa683744da66a7abef1019bb78
hongmaoxiao <769501294@qq.com>
2017-07-09 07:51:02 +0800

    use vue-cli

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Forget from '@/components/Forget';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'index',
+      component: Forget,
+    },
+  ],
+});
