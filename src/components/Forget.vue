90bb2912e7c4691d3ca4d65ac7fd85b2e6fe7739
hongmaoxiao <769501294@qq.com>
2017-07-10 07:46:41 +0800

    bind data to input

--- src/components/Forget.vue
+++ src/components/Forget.vue
@@ -30,10 +30,10 @@
       </mu-tbody>
     </mu-table>
     <mu-dialog :open="dialog" @close="close" title="新增提醒" scrollable>
-      <mu-text-field label="提醒标题" hintText="请输入标题" labelFloat v-modle="newSchedule" fullWidth required /><br/>
-      <mu-text-field label="提醒内容" hintText="请输入提醒内容" labelFloat fullWidth required /><br/>
-      <mu-date-picker hintText="请选择提醒日期"　fullWidth required /> <br/>
-      <mu-time-picker hintText="请选择提醒时间" format="24hr"　fullWidth required /><br/>
+      <mu-text-field label="提醒标题" hintText="请输入标题" labelFloat v-model="newSchedule.title" fullWidth required /><br/>
+      <mu-text-field label="提醒内容" hintText="请输入提醒内容" labelFloat v-model="newSchedule.events" fullWidth required /><br/>
+      <mu-date-picker hintText="请选择提醒日期"　v-model="newSchedule.selectDate" fullWidth required /> <br/>
+      <mu-time-picker hintText="请选择提醒时间" v-model="newSchedule.selectTime" format="24hr"　fullWidth required /><br/>
       <mu-raised-button label="取消"　primary slot="actions" @click="close" />
       <mu-raised-button label="确定" secondary slot="actions" @click="close" />
     </mu-dialog>
@@ -61,6 +61,12 @@ export default {
       enableSelectAll: true,
       showCheckbox: true,
       height: '300px',
+      newSchedule: {
+        title: '',
+        events: '',
+        selectDate: '',
+        selectTime: '',
+      },
     };
   },
   methods: {
