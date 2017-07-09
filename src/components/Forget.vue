5ac01c50e432c9c4752c4745d298eaa6099ef8c8
hongmaoxiao <769501294@qq.com>
2017-07-09 23:03:26 +0800

    add index

--- src/components/Forget.vue
+++ src/components/Forget.vue
@@ -1,22 +1,41 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div class="app">
+    <mu-appbar title="Title">
+      <router-link to="/" class="to-home">
+        首页
+      </router-link>
+      <mu-flat-button label="新增提醒" slot="right" @click="open" />
+      <mu-flat-button label="历史提醒" slot="right" />
+    </mu-appbar>
+    <mu-table :fixedFooter="fixedFooter" :fixedHeader="fixedHeader"
+      :enableSelectAll="enableSelectAll" :multiSelectable="multiSelectable"
+      :selectable="selectable" :showCheckbox="showCheckbox">
+      <mu-thead slot="header">
+        <mu-tr>
+          <mu-th tooltip="ID">ID</mu-th>
+          <mu-th tooltip="标题">标题</mu-th>
+          <mu-th tooltip="事情">事情</mu-th>
+          <mu-th tooltip="提醒时间">提醒时间</mu-th>
+          <mu-th tooltip="创建时间">创建时间</mu-th>
+        </mu-tr>
+      </mu-thead>
+      <mu-tbody>
+        <mu-tr v-for="item,index in tableData"  :key="index" :selected="item.selected">
+          <mu-td>{{index + 1}}</mu-td>
+          <mu-td>{{item.title}}</mu-td>
+          <mu-td>{{item.events}}</mu-td>
+          <mu-td>{{item.remindTime}}</mu-td>
+          <mu-td>{{item.createTime}}</mu-td>
+        </mu-tr>
+      </mu-tbody>
+    </mu-table>
+    <mu-dialog :open="dialog" @close="close" title="新增提醒" scrollable>
+      <mu-text-field label="标题" hintText="请输入标题" labelFloat /><br/>
+      <mu-text-field label="提醒内容" hintText="请输入提醒内容" labelFloat /><br/>
+      <mu-date-picker hintText="请选择日期"　/> <br/>
+      <mu-time-picker hintText="请选择时间" format="24hr"　/><br/>
+      <mu-flat-button primary label="关闭" @click="close" slot="actions"　/>
+    </mu-dialog>
   </div>
 </template>
 
@@ -24,30 +43,45 @@
 export default {
   name: 'forget',
   data() {
+    const menus = [];
+    for (let i = 0; i < 30; i += 1) {
+      menus.push(i + 1);
+    }
     return {
-      msg: 'Welcome to Your Vue.js App',
+      dialog: false,
+      menus,
+      tableData: [
+        {
+          title: '提交代码',
+          events: '下午记得提交代码',
+          remindTime: '2017-07-10 15:23:23',
+          createTime: '2017-07-10 10:23:23',
+        },
+      ],
+      fixedHeader: true,
+      fixedFooter: false,
+      selectable: true,
+      multiSelectable: true,
+      enableSelectAll: true,
+      showCheckbox: true,
+      height: '300px',
     };
+  },
+  methods: {
+    open() {
+      this.dialog = true;
+    },
+    close() {
+      this.dialog = false;
+    },
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+.to-home {
+  position: relative;
+  float: left;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import MuseUI from 'muse-ui';
+import 'muse-ui/dist/muse-ui.css';
 import Forget from '@/components/Forget';
 
 Vue.use(Router);
+Vue.use(MuseUI);
 
 export default new Router({
   routes: [
