e04f966cb74261297e7934426a0971f136bcec2f
hongmaoxiao <769501294@qq.com>
2017-09-24 22:44:15 +0800

    add global backToTop

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <mu-back-top class="back-to-top" />
   </div>
 </template>
 
@@ -18,5 +19,10 @@ export default {
   line-height: 1.5;
   color: #34495e;
   font-size: 14px;
+}
+
+.back-to-top .mu-back-up-default {
+  border-radius: 50%;
+  background-color: rgba(150, 150, 150, 0.6);
 }
 </style>
